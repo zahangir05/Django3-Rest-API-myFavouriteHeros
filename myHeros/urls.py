c00731e7690c1f28072eb2f16ef2afc1848e869b
Md. Zahangir Alam <zahangir05@gmail.com>
2020-10-03 00:33:31 +0600

    base template and navbar have been added to the project

--- myHeros/urls.py
+++ myHeros/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views
 
+# app_name = 'myHeros'
+
 router = routers.DefaultRouter()
 router.register('myHeros', views.HeroViewSet)
 urlpatterns = [
     path('', views.home, name='home'),
-    path('myHero', include(router.urls), name='myHerosAPI'),
+    path('myHero', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
